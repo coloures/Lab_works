49a7986c0aac06a5b3034662f9a8c0e333875fbd
Gleb_Bindiuk <69757295+coloures@users.noreply.github.com>
2024-04-06 13:00:14 +0600

    Add files via upload

---
+++ Алгоритмизация и программирование/Практики/Семестр_2_алгоритмизация/Номера телефонов/Phone_numbers_1_exercise.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace Dictionary_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++) 
+            {
+                queue.Enqueue(Console.ReadLine());
+            }
+            Dictionary<string, int> info = new Dictionary<string, int> ();
+            Hashtable ht = new Hashtable ();
+            while (queue.Count > 0)
+            {
+                string [] temp = queue.Dequeue().Split(' ');
+                if (info.ContainsKey(temp[0]))
+                {
+                    info[temp[0]] = info[temp[0]] + Convert.ToInt32(temp[3]);
+                    
+                }
+                else 
+                {
+                    info.Add(temp[0], Convert.ToInt32(temp[3]));
+                }
+                if (ht.ContainsKey(temp[0]))
+                {
+                    int prev = Convert.ToInt32(ht[temp[0]]);
+                    ht.Remove(temp[0]);
+                    ht.Add(temp[0], prev + Convert.ToInt32(temp[3]));
+                }
+                else 
+                {
+                    ht.Add(temp[0], Convert.ToInt32(temp[3]));
+                }
+            }
+            foreach(string inf in info.Keys) 
+            {
+                Console.WriteLine($"{inf} -> {info[inf]}");
+            }
+            foreach (string inf in ht.Keys)
+            {
+                Console.WriteLine($"{inf} -> {ht[inf]}");
+            }
+
+        }
+    }
+}
---
+++ Алгоритмизация и программирование/Практики/Семестр_2_алгоритмизация/Номера телефонов/Phone_numbers_2_exercise.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Dictionary_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                queue.Enqueue(Console.ReadLine());
+            }
+            Dictionary<string, int> info = new Dictionary<string, int>();
+            Hashtable ht = new Hashtable();
+            while (queue.Count > 0) 
+            {
+                string[] temp = queue.Dequeue().Split(' ');
+                if (info.ContainsKey(temp[1]))
+                {
+                    info[temp[1]] = info[temp[1]] + Convert.ToInt32(temp[3]);
+
+                }
+                else
+                {
+                    info.Add(temp[1], Convert.ToInt32(temp[3]));
+                }
+                if (ht.ContainsKey(temp[1]))
+                {
+                    int prev = Convert.ToInt32(ht[temp[1]]);
+                    ht.Remove(temp[1]);
+                    ht.Add(temp[1], prev + Convert.ToInt32(temp[3]));
+                }
+                else
+                {
+                    ht.Add(temp[1], Convert.ToInt32(temp[3]));
+                }
+            }
+            foreach (string inf in info.Keys)
+            {
+                Console.WriteLine($"{inf} -> {info[inf]}");
+            }
+            foreach (string inf in ht.Keys)
+            {
+                Console.WriteLine($"{inf} -> {ht[inf]}");
+            }
+        }
+    }
+}
